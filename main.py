7a0b49cdb07eb83a47d700bc912663694e8c9db2
Ctyring <75520657+Ctyring@users.noreply.github.com>
2021-02-24 20:02:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ session = requests.session()
 url = 'http://yiqing.ctgu.edu.cn/wx/index/loginSubmit.do'
 files = {'upload': open('test.txt', 'rb')}
 username = '2020112805'
-password = '*******'
+password = ${{ secrets.PASSWORD }}
 data = {
     'username': username,
     'password': password,
